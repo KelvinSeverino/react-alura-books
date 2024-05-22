2b9949a2066364ed08408b23d86c8c86f33d6d22
Kelvin Severino <kseveri@outlook.com>
2024-05-22 08:10:47 -0300

    Criação de Container com Styled Compoments

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,21 @@
-import './style.css'
-
 import Logo from '../Logo'
 import OptionsHeader from '../OptionsHeader'
 import IconsHeader from '../IconsHeader'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header = `
+    background-color: #FFF;
+    display: flex;
+    justify-content: center;
+`
 
 function Header(){
     return (
-        <header className='App-header'>
+        <HeaderContainer>
             <Logo></Logo>
             <OptionsHeader></OptionsHeader>
             <IconsHeader></IconsHeader>
-        </header>
+        </HeaderContainer>
     );
 }
 
